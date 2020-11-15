09e92568a8d914662afb3aa77ae32ee0518ffb17
Laubezout Théo <laubezouttheo@gmail.com>
2020-11-15 15:42:05 +0100

    Retrait des anciennes dépendances et ajout du rebuild automatique run-dev

--- indigo/run-dev.js
+++ indigo/run-dev.js
@@ -1,6 +1,29 @@
 const fs = require('fs-extra');
+const chokidar = require('chokidar');
 
+//Lecture de la config générale
+var configgjson = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
+var clockbuildlimite = 1;
+var clocksleep = configgjson.devtimesleepbuild;
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+//Execution du build de départ
 eval(fs.readFileSync(__dirname + "/build.js")+'');
 build("dev");
-//Message de fin de construction
-console.log("Build terminée !");
+
+//Ecoute des modifications du dossier sources
+chokidar.watch('./sources/site').on('all', (event, path) => {
+    if(clockbuildlimite == 0){
+      console.log(event, path);
+      build("dev");
+      console.log("Build fini");
+    }
+});
+  
+sleep(clocksleep).then(() => {
+    clockbuildlimite = 0;
+    console.log("Build terminée, bon code !");
+});
