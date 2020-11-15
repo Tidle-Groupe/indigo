8f832c98f24e4ad0b1f34e5ad80b0b6ffb96905f
Laubezout Théo <laubezouttheo@gmail.com>
2020-11-15 19:58:36 +0100

    Ajout de l'extension php pdo_postgresql à l'image docker

--- sources/api/interne/config/database.php
+++ sources/api/interne/config/database.php
@@ -5,6 +5,7 @@ $mariadb = new PDO("mysql:host=db;dbname=api_interne;charset=utf8", "root", 'adm
 //Déclaration MongoDB
 
 //Déclaration Postgres
+$postgres = new PDO("pgsql:host=postgres;port=5432;dbname=postgres;user=postgres;password=admin");
 
 //Déclaration Redis
 $redis = new Redis();
