dde69b2d00905974131ab0ff2cc3e9fb4b2038fb
Laubezout Théo <laubezouttheo@gmail.com>
2020-11-15 21:04:51 +0100

    Ajout de l'extension php mongodb à l'image docker

--- sources/api/interne/config/database.php
+++ sources/api/interne/config/database.php
@@ -3,6 +3,8 @@
 $mariadb = new PDO("mysql:host=db;dbname=api_interne;charset=utf8", "root", 'admin');
 
 //Déclaration MongoDB
+$mongodb = new MongoDB\Driver\Manager("mongodb://root:admin@mongo/api_intern"); 
+
 
 //Déclaration Postgres
 $postgres = new PDO("pgsql:host=postgres;port=5432;dbname=postgres;user=postgres;password=admin");
