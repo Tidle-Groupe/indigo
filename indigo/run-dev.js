7c58b8ccec34ff41f4e180af2906d8eada2422fe
Laubezout Théo <laubezouttheo@gmail.com>
2020-11-17 18:00:00 +0100

    Ajout d'une sécurité pour ne pas relancer le run-dev pendant un build

--- indigo/run-dev.js
+++ indigo/run-dev.js
@@ -1,42 +1,48 @@
 const fs = require('fs-extra');
 const chokidar = require('chokidar');
 
-//Lecture de la config générale
-var configgjson = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-
-var build = "dev";
-
-var clockbuildlimite = 1;
-var clocksleep = configgjson.devtimesleepbuild;
-const sleep = (milliseconds) => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
-
 //Execution du build de départ
 eval(fs.readFileSync(__dirname + "/build.js")+'');
-var build_bib_js = build_site("dev");
+//Comptage du temps du build de départ
+var timebuildsite = Date.now();
+build_bib_js = build_site("dev");
+var timebuildsite = Date.now()-timebuildsite;
+var timebuildassets = Date.now();
 build_assets("dev", build_bib_js);
+var timebuildassets = Date.now()-timebuildassets;
 build_api("dev");
+watchfilebuild(timebuildsite, timebuildassets);
 
-//Ecoute des modifications du dossier sources site
-chokidar.watch('./sources/site').on('all', (event, path) => {
-    if(clockbuildlimite == 0){
+//Fonction d'execution du build lors de modifications
+function watchfilebuild(timebuildinitsite, timebuildinitassets){
+  timebuildsite = timebuildinitsite;
+  timebuildassets = timebuildinitassets;
+  clocklastsite = Date.now();
+  clocklastassets = Date.now();
+
+  //Ecoute des modifications du dossier sources site
+  chokidar.watch('./sources/site').on('all', (event, path) => {
+    if(Number(clocklastsite+timebuildsite+500) < Date.now()){
       console.log(event, path);
+      //Comptage du nouveau temps de build
+      var clock = Date.now();
       build_site("dev");
+      timebuildsite = Date.now()-clock;
       console.log("Build site fini");
+      clocklastsite = Date.now();
     }
-});
+  });
 
-//Ecoute des modifications du dossier sources assets
-chokidar.watch('./sources/assets').on('all', (event, path) => {
-  if(clockbuildlimite == 0){
-    console.log(event, path);
-    build_assets("dev", build_bib_js);
-    console.log("Build assets fini");
-  }
-});
-  
-sleep(clocksleep).then(() => {
-    clockbuildlimite = 0;
-    console.log("Build terminée, bon code !");
-});
+  //Ecoute des modifications du dossier sources assets
+  chokidar.watch('./sources/assets').on('all', (event, path) => {
+    if(Number(clocklastassets+timebuildassets+500) < Date.now()){
+      console.log(event, path);
+      //Comptage du nouveau temps de build
+      var clock = Date.now();
+      build_assets("dev", build_bib_js);
+      timebuildassets = Date.now()-clock;
+      console.log("Build assets fini");
+      clocklastassets = Date.now();
+    }
+  });
+}
