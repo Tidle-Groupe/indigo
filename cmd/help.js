fee1c97740032ec7b136f51b33268be87850df60
Théo Laubezout <laubezouttheo@gmail.com>
2020-11-23 16:06:14 +0100

    Ajout commande help

---
+++ cmd/help.js
@@ -0,0 +1,14 @@
+//Gestion du retour help
+var help = `
+      indigo [commande] <options>
+  
+      new ................ créer un nouvel espace de travail indigo
+      build .............. créer un build à partir des fichiers de l'espace de travail indigo courant
+      run ................ lance un serveur local à partir de votre espace de travail indifo courant
+      docker ............. permet de gérer les conteneurs docker qui lancent le serveur local
+      help ............... permet d'afficher ce texte
+      
+      Pour plus d'informations, reportez-vous à la documentation:
+      https://github.com/Tidle-Groupe/indigo/wiki`;
+      
+console.log(help);
