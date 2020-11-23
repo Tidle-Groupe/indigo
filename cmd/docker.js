8d144aef4da3b5a0ee4e7607c70fc60a5516d870
Théo Laubezout <laubezouttheo@gmail.com>
2020-11-24 00:11:48 +0100

    Ajout d'une aide pour la commande docker

--- cmd/docker.js
+++ cmd/docker.js
@@ -6,10 +6,28 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-//Fonction de démarrage
+//Fonction docker compose
 function docker_compose(cmd){
-    //Execution du docker-compose start
+    //Execution du docker-compose
+    console.log("Execution de l'action...");
     exec('docker-compose -f docker.yml '+cmd).toString();
+}
+
+//Fonction d'affichage du help
+function help(){
+    var help = `
+      indigo docker <options>
+  
+      start .............. démarre les conteneurs docker de l'espace de travail indigo courant
+      stop ............... arrête les conteneurs docker de l'espace de travail indigo courant
+      install ............ installe les conteneurs docker de l'espace de travail indigo courant
+      uninstall .......... supprime les conteneurs docker de l'espace de travail indigo courant
+      help ............... permet d'afficher cette aide
+      
+      Pour plus d'informations, reportez-vous à la documentation:
+      https://github.com/Tidle-Groupe/indigo/wiki`;
+      
+    console.log(help);
 }
 
 //Fonction de routage
@@ -27,19 +45,25 @@ function routage_choix(choix){
         case 'stop':
             docker_compose("stop");
             break;
+        //Commande install
+        case 'install':
+            docker_compose("up -d");
+            break;
         //Commande uninstall
         case 'uninstall':
             docker_compose("down");
             break;
         //Affichage du help de la commande
         default:
+            help();
     }
 
 }
 
 //Demande de l'action à effectuer sur les conteneurs
 console.log("Quelle action effectuer ?");
-console.log("start, stop, uninstall, help");
+console.log("start, stop, install, uninstall, help");
 rl.question('', (choix) => {
     routage_choix(choix);
+    rl.close();
 });
--- cmd/help.js
+++ cmd/help.js
@@ -5,8 +5,8 @@ var help = `
       new ................ créer un nouveau projet indigo
       build .............. créer un build à partir des fichiers de l'espace de travail indigo courant
       run ................ lance un serveur local à partir de votre espace de travail indigo courant
-      docker ............. permet de gérer les conteneurs docker pour chaques projets
-      help ............... permet d'afficher ce texte
+      docker ............. permet de gérer les conteneurs docker de l'espace de travail indigo courant
+      help ............... permet d'afficher cette aide
       
       Pour plus d'informations, reportez-vous à la documentation:
       https://github.com/Tidle-Groupe/indigo/wiki`;
