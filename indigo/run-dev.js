a6f6d9c3bfafe0d7d1be53f9cd0e7c2ebdc8648a
Théo Laubezout <laubezouttheo@gmail.com>
2020-11-25 00:32:13 +0100

    Ajout de la vérification dossier sources existe

--- indigo/run-dev.js
+++ indigo/run-dev.js
@@ -34,49 +34,52 @@ build.init("dev");
 //Comptage du temps du build de départ
 var timebuildsite = Date.now();
 build_bib_js = build.build_site("dev");
-var timebuildsite = Date.now()-timebuildsite;
-var timebuildassets = Date.now();
-build.build_assets("dev", build_bib_js);
-var timebuildassets = Date.now()-timebuildassets;
-build.build_api("dev");
-watchfilebuild(timebuildsite, timebuildassets);
-//Message de fin de construction
-console.log("Build terminée !");
+//si on à pas de retour d'erreur
+if(build_bib_js !== 'error'){
+  var timebuildsite = Date.now()-timebuildsite;
+  var timebuildassets = Date.now();
+  build.build_assets("dev", build_bib_js);
+  var timebuildassets = Date.now()-timebuildassets;
+  build.build_api("dev");
+  watchfilebuild(timebuildsite, timebuildassets);
+  //Message de fin de construction
+  console.log("Build terminée !");
 
-//Fonction d'execution du build lors de modifications
-function watchfilebuild(timebuildinitsite, timebuildinitassets){
-  timebuildsite = timebuildinitsite;
-  timebuildassets = timebuildinitassets;
-  clocklastsite = Date.now();
-  clocklastassets = Date.now();
+  //Fonction d'execution du build lors de modifications
+  function watchfilebuild(timebuildinitsite, timebuildinitassets){
+    timebuildsite = timebuildinitsite;
+    timebuildassets = timebuildinitassets;
+    clocklastsite = Date.now();
+    clocklastassets = Date.now();
 
-  //Ecoute des modifications du dossier sources site
-  chokidar.watch('./sources/site').on('all', (event, path) => {
-    if(Number(clocklastsite+timebuildsite+500) < Date.now()){
-      console.log(event, path);
-      //Message de début de construction
-      console.log("Début du build !");
-      //Comptage du nouveau temps de build
-      var clock = Date.now();
-      build.build_site("dev");
-      timebuildsite = Date.now()-clock;
-      console.log("Build site fini");
-      clocklastsite = Date.now();
-    }
-  });
+    //Ecoute des modifications du dossier sources site
+    chokidar.watch('./sources/site').on('all', (event, path) => {
+      if(Number(clocklastsite+timebuildsite+500) < Date.now()){
+        console.log(event, path);
+        //Message de début de construction
+        console.log("Début du build !");
+        //Comptage du nouveau temps de build
+        var clock = Date.now();
+        build.build_site("dev");
+        timebuildsite = Date.now()-clock;
+        console.log("Build site fini");
+        clocklastsite = Date.now();
+      }
+    });
 
-  //Ecoute des modifications du dossier sources assets
-  chokidar.watch('./sources/assets').on('all', (event, path) => {
-    if(Number(clocklastassets+timebuildassets+500) < Date.now()){
-      console.log(event, path);
-      //Message de début de construction
-      console.log("Début du build !");
-      //Comptage du nouveau temps de build
-      var clock = Date.now();
-      build.build_assets("dev", build_bib_js);
-      timebuildassets = Date.now()-clock;
-      console.log("Build assets fini");
-      clocklastassets = Date.now();
-    }
-  });
+    //Ecoute des modifications du dossier sources assets
+    chokidar.watch('./sources/assets').on('all', (event, path) => {
+      if(Number(clocklastassets+timebuildassets+500) < Date.now()){
+        console.log(event, path);
+        //Message de début de construction
+        console.log("Début du build !");
+        //Comptage du nouveau temps de build
+        var clock = Date.now();
+        build.build_assets("dev", build_bib_js);
+        timebuildassets = Date.now()-clock;
+        console.log("Build assets fini");
+        clocklastassets = Date.now();
+      }
+    });
+  }
 }
